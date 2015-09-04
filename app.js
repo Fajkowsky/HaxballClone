d651269943595af2051cd51893ee25ac9bb15c23
Fajkowsky <dawidfajkowski@gmail.com>
2015-09-04 20:32:57 +0200

    working on sprites and movment

--- app.js
+++ app.js
@@ -4,8 +4,20 @@
         HEIGHT: 800,
         WIDTH: 600
     };
-
     window.onload = function () {
-        var game = new Phaser.Game(config.HEIGHT, config.WIDTH, Phaser.AUTO, 'gameContainer');
+        var game = new Phaser.Game(config.HEIGHT, config.WIDTH, Phaser.AUTO, '', {
+                preload: function () {
+                    this.load.image('player', 'assets/images/ball.png');
+                },
+                create: function () {
+                    this.stage.backgroundColor = '#939e7f';
+                    this.ball = this.game.add.sprite(this.world.centerX, this.world.centerY, 'player');
+                },
+                update: function () {
+                    if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+                        this.ball.velocity = -5;
+                    }
+                }
+            });
     };
 }());
