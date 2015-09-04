8efdc04b56890f51dd3f53e2b356f6e6c40477f5
Fajkowsky <dawidfajkowski@gmail.com>
2015-09-04 20:52:59 +0200

    basic movement is working

--- app.js
+++ app.js
@@ -6,18 +6,28 @@
     };
     window.onload = function () {
         var game = new Phaser.Game(config.HEIGHT, config.WIDTH, Phaser.AUTO, '', {
-                preload: function () {
-                    this.load.image('player', 'assets/images/ball.png');
-                },
-                create: function () {
-                    this.stage.backgroundColor = '#939e7f';
-                    this.ball = this.game.add.sprite(this.world.centerX, this.world.centerY, 'player');
-                },
-                update: function () {
-                    if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-                        this.ball.velocity = -5;
-                    }
+            preload: function () {
+                this.load.image('player', 'assets/images/ball.png');
+            },
+            create: function () {
+                this.stage.backgroundColor = '#939e7f';
+                this.ball = this.game.add.sprite(this.world.centerX, this.world.centerY, 'player');
+                this.cursors = this.game.input.keyboard.createCursorKeys();
+            },
+            update: function () {
+                if (this.cursors.left.isDown) {
+                    this.ball.x -= 5;
                 }
-            });
+                if (this.cursors.right.isDown) {
+                    this.ball.x += 5;
+                }
+                if (this.cursors.up.isDown) {
+                    this.ball.y -= 5;
+                }
+                if (this.cursors.down.isDown) {
+                    this.ball.y += 5;
+                }
+            }
+        });
     };
 }());
