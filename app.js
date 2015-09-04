768ed7730a4c66c08f208cf4a2cac592936d402d
Fajkowsky <dawidfajkowski@gmail.com>
2015-09-04 21:23:37 +0200

    replace sprites and working on logic

--- app.js
+++ app.js
@@ -7,25 +7,31 @@
     window.onload = function () {
         var game = new Phaser.Game(config.HEIGHT, config.WIDTH, Phaser.AUTO, '', {
             preload: function () {
-                this.load.image('player', 'assets/images/ball.png');
+                this.load.image('bluePlayer', 'assets/images/blue_player.png');
+                this.load.image('bluePlayerShot', 'assets/images/blue_player_shot.png');
             },
             create: function () {
                 this.stage.backgroundColor = '#939e7f';
-                this.ball = this.game.add.sprite(this.world.centerX, this.world.centerY, 'player');
+                this.bluePlayer = this.game.add.sprite(this.world.centerX, this.world.centerY, 'bluePlayer');
                 this.cursors = this.game.input.keyboard.createCursorKeys();
             },
             update: function () {
                 if (this.cursors.left.isDown) {
-                    this.ball.x -= 5;
+                    this.bluePlayer.x -= 5;
                 }
                 if (this.cursors.right.isDown) {
-                    this.ball.x += 5;
+                    this.bluePlayer.x += 5;
                 }
                 if (this.cursors.up.isDown) {
-                    this.ball.y -= 5;
+                    this.bluePlayer.y -= 5;
                 }
                 if (this.cursors.down.isDown) {
-                    this.ball.y += 5;
+                    this.bluePlayer.y += 5;
+                }
+                if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+                    this.bluePlayer.loadTexture('bluePlayerShot');
+                } else {
+                    this.bluePlayer.loadTexture('bluePlayer');
                 }
             }
         });
