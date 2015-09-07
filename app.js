aa3ec26ea5cb9b259c2a21f28a9527f77a2d35ac
Fajkowsky <dawidfajkowski@gmail.com>
2015-09-07 19:14:21 +0200

    working on looking like haxball

--- app.js
+++ app.js
@@ -7,33 +7,14 @@
     window.onload = function () {
         var game = new Phaser.Game(config.HEIGHT, config.WIDTH, Phaser.AUTO, 'haxball', {
             preload: function () {
-                this.load.image('bluePlayer', 'assets/images/blue_player.png');
-                this.load.image('bluePlayerShot', 'assets/images/blue_player_shot.png');
+                this.load.image('field', 'assets/images/field.png');
             },
             create: function () {
                 this.stage.backgroundColor = '#3b302a';
 
-                this.bluePlayer = this.game.add.sprite(this.world.centerX, this.world.centerY, 'bluePlayer');
-                this.cursors = this.game.input.keyboard.createCursorKeys();
+                this.field = this.add.sprite(10, 35, 'field');
             },
             update: function () {
-                if (this.cursors.left.isDown) {
-                    this.bluePlayer.x -= 5;
-                }
-                if (this.cursors.right.isDown) {
-                    this.bluePlayer.x += 5;
-                }
-                if (this.cursors.up.isDown) {
-                    this.bluePlayer.y -= 5;
-                }
-                if (this.cursors.down.isDown) {
-                    this.bluePlayer.y += 5;
-                }
-                if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-                    this.bluePlayer.loadTexture('bluePlayerShot');
-                } else {
-                    this.bluePlayer.loadTexture('bluePlayer');
-                }
             }
         });
     };
